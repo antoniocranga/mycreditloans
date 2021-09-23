8f903564cc123a9f48601d270fd3ee2fbfb3844d
Antonio <antocranga@gmail.com>
2021-09-23 22:14:52 +0300

    Constants added

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+const Color lightGrey = Color(0xff999999);
+const Color greenColor = Color(0xff229E00);
+const Color redColor = Color(0xffFF6B6B);
+const Color logoColor = Color(0xff0079EB);
+const Color logoColorAccent = Color(0xff64a7ff);
+const Color scaffoldBackgroundColor = Color(0xffF8F8F8);
+const Color darkTextColor = Color(0xFFB0B0B0);
+
+const double kSplashRadius = 18.0;
+const double kCustomToolbarHeight = 50.0;
